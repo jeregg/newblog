668767826983ef5b080fee47ee4eb4ee88a86382
JEREGG T <2405348624@qq.com>
2023-06-10 08:08:10 +0000

    font weight adjustment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7ea7267a8fcfb581"; }
+/******/ 		__webpack_require__.h = function() { return "2edb8d990742c586"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
