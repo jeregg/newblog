2057d2210bd32704f14e7201333a5b9a14919807
JEREGG <2405348624@qq.com>
2023-08-20 08:32:45 +0000

    Make other changes.

--- components/sidebar.js
+++ components/sidebar.js
@@ -24,7 +24,7 @@ export default function Navbar() {
   }
 
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="-ml-[8px] mb-12 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <LayoutGroup>
           <nav
