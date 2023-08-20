74fda2c77cc14cbe696ba34cdb72866e694e0121
JEREGG <2405348624@qq.com>
2023-08-20 08:32:05 +0000

    Make some updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,6 @@ module.exports = {
   content: ['./pages/*.js','./components/*.js'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['var(--font-graphik)'],
-      },
-      typography: {
-        quoteless: {
-          css: {
-            'blockquote p:first-of-type::before': { content: 'none' },
-            'blockquote p:first-of-type::after': { content: 'none' },
-          },
-        },
-      },
     },
   },
   plugins: [],
