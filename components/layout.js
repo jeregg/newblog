c6af43ed6f90efeff23b4af2202acde15729c0de
JEREGG <2405348624@qq.com>
2023-08-20 09:24:16 +0000

    Adjust mx-3

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout(props) {
       lang="en"
       className="text-black bg-white dark:text-white dark:bg-[#111010]"
     >
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-2 mt-4 lg:mx-auto">
+      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-3 mt-4 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-4 flex flex-col">
           <Sidebar />
           {children}
