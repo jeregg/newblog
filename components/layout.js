2dc9d2505fc15f2c150058c14383ce1f1d62076f
JEREGG <2405348624@qq.com>
2023-08-20 14:04:40 +0800

    Add files via upload

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,46 @@
-import Header from '../components/header'
-import Footer from '../components/footer'
+import clsx from 'clsx';
+import localFont from '@next/font/local';
+import Sidebar from '../components/sidebar';
+
+const graphik = localFont({
+  src: [
+    {
+      path: '../public/fonts/Graphik-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Graphik-Medium.ttf',
+      weight: '600',
+      style: 'bold',
+    },
+  ],
+  variable: '--font-graphik',
+  display: 'swap',
+});
 
 export const metadata = {
   title: 'PLSFIX',
   description: 'PLSFIX',
 }
 
-const Layout = ({ children }) => {
-  return (
-    <div className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8">
-          <aside className="md:w-[120px] md:flex-shrink-0 md:mx-0 md:px-0"><Header /></aside>
-          <main className="flex-auto min-w-0 md:mt-0 flex flex-col">{ children }</main>
-    </div>
-  )
-}
 
-export default Layout;
+export default function RootLayout(props) {
+  const { children } = props;
+  return (
+    <html
+      lang="en"
+      className={clsx(
+        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        graphik.variable
+      )}
+    >
+      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-4 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Sidebar />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
