1ef80c24d843a9959802ebe9f55ce09fb8fe7645
JEREGG <2405348624@qq.com>
2023-08-20 14:48:01 +0800

    Add files via upload

--- components/layout.js
+++ components/layout.js
@@ -1,23 +1,5 @@
-import clsx from 'clsx';
-import localFont from '@next/font/local';
 import Sidebar from '../components/sidebar';
 
-const graphik = localFont({
-  src: [
-    {
-      path: '../public/fonts/Graphik-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Graphik-Medium.ttf',
-      weight: '600',
-      style: 'bold',
-    },
-  ],
-  variable: '--font-graphik',
-  display: 'swap',
-});
 
 export const metadata = {
   title: 'PLSFIX',
@@ -30,10 +12,7 @@ export default function RootLayout(props) {
   return (
     <html
       lang="en"
-      className={clsx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
-        graphik.variable
-      )}
+      className="text-black bg-white dark:text-white dark:bg-[#111010]"
     >
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-4 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
